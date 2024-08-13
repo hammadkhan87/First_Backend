4e1fe04520ae41b44c41f6d916067440b942f8ae
hammadkhan <86566345+hammadkhan87@users.noreply.github.com>
2024-08-13 04:11:27 +0400

    utilscode dic

--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ dotenv.config({
     path:"./env"
 })
 const app =express()
-connectDB()
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log("server is running on port")
+    })
+}).catch((err)=>{console.log("db connection error",err)})
 
 
 
